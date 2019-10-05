e845c72f98faf7b6a97598517a7c45f8497969dd
TriDivide <liamsmitha@gmail.com>
2019-10-05 17:39:11 +0100

    - fixed a bug with converting double to float.

--- Assets/DataModel/Models/LevelModel.cs
+++ Assets/DataModel/Models/LevelModel.cs
@@ -12,7 +12,7 @@ public sealed class LevelModel {
     }
 
     private LevelModel() {
-        Mission simpleMission = new Mission(id: 0, name: "Beg", description: "To earn enough money to start you own adventurers guild, you decide to that begging in the streets is a good idea. Got to start somewhere I guess...", successChance: 0.95, goldReward: 10, difficulty: Mission.DifficultyType.Easy, metalReward: null, lumberReward: null, timer: 120, completed: false, successMessage: "Congratulations you managed to scrounge up some gold pieces off of some charitable citizens that have clearly done more with their lives that you. ", failureMessage: "While begging for money, your adventurer took an axe to the head for looking at looking at an angly Dwarf funny. Thier mother must be so proud.");
+        Mission simpleMission = new Mission(id: 0, name: "Beg", description: "To earn enough money to start you own adventurers guild, you decide to that begging in the streets is a good idea. Got to start somewhere I guess...", successChance: 0.95f, goldReward: 10, difficulty: Mission.DifficultyType.Easy, metalReward: null, lumberReward: null, timer: 120f, completed: false, successMessage: "Congratulations you managed to scrounge up some gold pieces off of some charitable citizens that have clearly done more with their lives that you. ", failureMessage: "While begging for money, your adventurer took an axe to the head for looking at looking at an angly Dwarf funny. Thier mother must be so proud.");
         missionList[simpleMission.ID] = simpleMission;
     }
 
